314402006a5be105642a42b5396ed8cdea371cf7
sssh11035005 <104436707+sssh11035005@users.noreply.github.com>
2022-05-17 20:29:56 +0800

    Update ex4-1.cpp

--- ex4-1.cpp
+++ ex4-1.cpp
@@ -1,1 +1,22 @@
-123
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(){
+    int x,y;
+    int bool_value;
+    
+    printf("please input x:\n");
+    scanf("%d",&x);
+    printf("please input y:\n");
+    scanf("%d",&y);
+    
+    bool_value = (x>y);
+    printf("x>y ==> %d \n" ,bool_value);
+    bool_value = (x<y);
+    printf("x<y ==> %d \n" ,bool_value);
+    bool_value = (x<5 && x<10);
+    printf("x<5 && x<10 ==> %d \n" ,bool_value);
+    cout<<"Hello World";
+
+    return 0;
+}
