f85f09ac6ee50c4e580e7d1a0b1d7d9504e5c1e9
sssh11035005 <104436707+sssh11035005@users.noreply.github.com>
2022-05-17 20:43:55 +0800

    Create ex4-3.cpp

---
+++ ex4-3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main(){
+    int x,y;
+    int bool_value;
+    
+    printf("please input x:\n");
+    scanf("%d",&x);
+    printf("please input y:\n");
+    scanf("%d",&y);
+    
+    if (x>y){
+         printf("x>y ==> %d \n" ,x>y);}
+    else if(x<y){
+        printf("x<y ==> %d \n" ,x<y);}
+    else {
+        printf("x == y \n");}
+    return 0;
+}
