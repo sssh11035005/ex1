fc28c89a925d41e6938558f2edc9a50cddf06b76
sssh11035005 <104436707+sssh11035005@users.noreply.github.com>
2022-05-24 20:59:18 +0800

    Create ex5-3.cpp

---
+++ ex5-3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int N;
+    int i, j;
+    cin >> N;
+    for( i=1 ; i<=N ; i=i+1 )
+    {
+        for( j=1 ; j<=0 ; j=j+1 )
+        {
+            cout << " ";
+        }
+        for( j=1 ; j<=2*i-1 ; j=j+1 )
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
