ed5687451f0857ce074cebcdc30968441fa694ce
sssh11035005 <104436707+sssh11035005@users.noreply.github.com>
2022-05-24 21:17:04 +0800

    Update ex5-2.cpp

--- ex5-2.cpp
+++ ex5-2.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include<iomanip>
 using namespace std;
 int main()
@@ -11,4 +11,21 @@ int main()
         cout<<"\n";
         }
     }
+}
+*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 9; i++)
+    {
+        for (j = 1; j <= 9; j++)
+        {
+            cout << j << "*" << i << "=" << j * i << "\t ";
+         }
+        cout << endl;
+    }
+    return 0;
 }
