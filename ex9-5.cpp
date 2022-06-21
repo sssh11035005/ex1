c9de62b4a3a5fff7293b476a87556ace87eabd43
sssh11035005 <104436707+sssh11035005@users.noreply.github.com>
2022-06-21 21:42:15 +0800

    Update ex9-5.cpp

--- ex9-5.cpp
+++ ex9-5.cpp
@@ -18,3 +18,29 @@ int main(){
     
     return 0;
 }
+
+/*************************
+#include <iostream>
+using namespace std;
+void print_array(int* p,int n){
+    int i,n1 = sizeof(p) / sizeof(int);
+    printf(" in function, array size n = %d \n",n1);
+    for(i=0;i<n;i++){
+        printf("%d , ",p[i]);
+        //printf("%d , ",*(p+1));
+    }
+    printf("\n");
+}
+int main(){
+    int a[]={9,10,6,3,2};
+    int n = sizeof(p) / sizeof(int);
+    int*p = a;
+    printf(" array size n = %d \n",n);
+    print_array(p,n);
+    
+    for(i=0;i<n;i++){
+        printf("a[%d]=%d \n",i,*(p+i));
+    }
+    return 0;
+}
+****************************/
